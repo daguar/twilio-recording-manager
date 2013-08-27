f5d3027fe2fa65967ae173ecee043a55ea67c5fb
Dave Guarino <dave@codeforamerica.org>
2013-08-26 23:50:28 -0700

    Add total # of recordings and some more columns

--- twilio_recordings_manager.rb
+++ twilio_recordings_manager.rb
@@ -1,9 +1,9 @@
 require 'twilio-ruby'
 require 'sinatra'
 
-# Development
+# For development
 require 'pry'
-require 'vcr'
+#require 'vcr'
 
 ACCOUNT_SID = ENV['TWILIO_SID']
 AUTH_TOKEN = ENV['TWILIO_AUTH']
@@ -12,9 +12,10 @@ CLIENT = Twilio::REST::Client.new ACCOUNT_SID, AUTH_TOKEN
 get '/list' do
   begin
     @recordings_list = CLIENT.account.recordings.list
-    @rendered_html = "<table border='2'><tr><td>Call SID</td><td>Length</td></tr>"
+    @rendered_html = "<h2>Total # of recordings: #{@recordings_list.total}<h2>"
+    @rendered_html += "<table border='2'><tr><td>Call SID</td><td>Length</td><td>Date Created</td></tr>"
     @recordings_list.each do |recording|
-      @rendered_html += "<tr><td><a href='#{recording.mp3}'>#{recording.call_sid}</a></td><td>#{recording.duration}s</td><tr>"
+      @rendered_html += "<tr><td><a href='#{recording.mp3}'>#{recording.call_sid}</a></td><td>#{recording.duration}s</td><td>#{recording.date_created}</td><tr>"
     end
     @rendered_html += "</table>"
     @rendered_html
